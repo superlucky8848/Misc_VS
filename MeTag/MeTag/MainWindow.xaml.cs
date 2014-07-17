7ae8eb55c7e4d5ae4d18cbf10faccf54e18d5920
superlucky <mail.superlucky@gmail.com>
2014-07-17 11:53:42 +0800

    Add MeTag A WPF Tool for taggin

---
+++ MeTag/MeTag/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace MeTag
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            titleBar.MouseLeftButtonDown += (o, e) => DragMove();
+            
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            tbTitle.Text = "MeTag[v" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString() + "]";
+        }
+
+        private void btExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+    }
+}
